16ed74804fc2b6e5f1434eb57ba1e3e6d7cd9d92
Joaquín Ramírez <50754596+lordjuacs@users.noreply.github.com>
2021-12-18 08:12:10 -0600

    Update error-connect.sh

--- hito2/error-connect.sh
+++ hito2/error-connect.sh
@@ -1,15 +1,15 @@
 py "Hosts imposibles de conectar"
 py "h1 --> h3"
 h1 ping -c 4 h3
-py "h2 --> h1"
+py "h3 --> h1"
 h3 ping -c 4 h1
 
 py "h4 --> h5"
-h3 ping -c 4 h4
+h4 ping -c 4 h5
 py "h5 --> h4"
-h4 ping -c 4 h3
+h5 ping -c 4 h4
 
 py "h7 --> h2"
-h7 ping -c 4 h5
+h7 ping -c 4 h2
 py "h2 --> h7"
-h7 ping -c 4 h6
+h2 ping -c 4 h7
